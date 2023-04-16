ca89270168fcba113b5d5d08052a70aae0016b27
yuri0x7c1 <yuri0x7c1@gmail.com>
2023-04-16 15:13:58 +0300

    Update header's and footer's styles

--- src/main/resources/META-INF/resources/frontend/bs-card/bs-card-footer.css.js
+++ src/main/resources/META-INF/resources/frontend/bs-card/bs-card-footer.css.js
@@ -2,16 +2,11 @@
 import { css } from 'lit';
 
 export const BsCardFooterCss = css`
-
     :host {
         padding: 0.75rem 1.25rem;
-        background-color: rgba(0, 0, 0, 0.03);
-        border-top: 1px solid rgba(0, 0, 0, 0.125);
-        border-bottom-left-radius: var(--card-footer-bottom-left-border-radius);
-        border-bottom-right-radius: var(--card-footer-bottom-right-border-radius);
-    }
-
-    :host ::slotted(*:last-child) {
-        border-radius: 0 0 calc(0.25rem - 1px) calc(0.25rem - 1px);
+        margin-bottom: 0;
+        background-color: var(--bs-card-footer-background-color, var(--lumo-contrast-5pct));
+        border-bottom: var(--bs-card-border, 1px solid var(--lumo-contrast-20pct));
+        border-radius: var(--bs-card-border-radius, 0px);
     }
 `;
--- src/main/resources/META-INF/resources/frontend/bs-card/bs-card-header.css.js
+++ src/main/resources/META-INF/resources/frontend/bs-card/bs-card-header.css.js
@@ -2,34 +2,11 @@
 import { css } from 'lit';
 
 export const BsCardHeaderCss = css`
-
     :host {
         padding: 0.75rem 1.25rem;
         margin-bottom: 0;
-        background-color: rgba(0, 0, 0, 0.03);
-        border-bottom: 1px solid rgba(0, 0, 0, 0.125);
-        border-top-left-radius: var(--card-header-top-left-border-radius);
-        border-top-right-radius: var(--card-header-top-right-border-radius);
-    }
-
-    :host:first-child {
-        border-radius: calc(0.25rem - 1px) calc(0.25rem - 1px) 0 0;
-    }
-
-    :host ::slotted(*) + .list-group .list-group-item:first-child {
-        border-top: 0;
-    }
-
-    :host ::slotted(bs-nav[tabs]) {
-        --bs-nav-margin-right: -0.625rem;
-        --bs-nav-margin-bottom: -0.75rem;
-        --bs-nav-margin-left: -0.625rem;
-        --bs-nav-border-bottom: 0;
-        --nav-link-active-background-color: #fff;
-    }
-
-    :host ::slotted(bs-nav[pills]) {
-        --bs-nav-margin-right: -0.625rem;
-        --bs-nav-margin-left: -0.625rem;
+        background-color: var(--bs-card-header-background-color, var(--lumo-contrast-5pct));
+        border-bottom: var(--bs-card-border, 1px solid var(--lumo-contrast-20pct));
+        border-radius: var(--bs-card-border-radius, 0px);
     }
 `;
